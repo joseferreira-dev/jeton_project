c83578310f1b43762b79bf56631ed06b7e9c55db
José Ferreira <joseguedes.dev@gmail.com>
2026-06-02 08:10:29 -0300

    Remove imports desnecessários

--- src/main/java/br/com/cremepe/jeton/servico/AtividadeConselhalService.java
+++ src/main/java/br/com/cremepe/jeton/servico/AtividadeConselhalService.java
@@ -9,8 +9,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
