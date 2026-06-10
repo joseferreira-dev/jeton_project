448c1e2ed0154d8c428ffa445fb63e4116407b52
José Ferreira <joseguedes.dev@gmail.com>
2026-06-10 10:05:18 -0300

    Refatora ParametrosService

--- src/main/java/br/com/cremepe/jeton/service/ParametrosService.java
+++ src/main/java/br/com/cremepe/jeton/service/ParametrosService.java
@@ -3,7 +3,6 @@ package br.com.cremepe.jeton.service;
 import br.com.cremepe.jeton.annotation.Auditar;
 import br.com.cremepe.jeton.domain.Parametros;
 import br.com.cremepe.jeton.repository.ParametrosRepository;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,24 +17,22 @@ public class ParametrosService {
     @Autowired
     private ParametrosRepository repository;
 
-    @Transactional(readOnly = true)
-    public boolean isSistemaBloqueado() {
-        Parametros params = repository.findById(1).orElseGet(() -> {
+    private Parametros getParametros() {
+        return repository.findById(1).orElseGet(() -> {
             Parametros novo = new Parametros();
             novo.setBloqueaSistema("N");
             return repository.save(novo);
         });
-        return "S".equals(params.getBloqueaSistema());
+    }
+
+    @Transactional(readOnly = true)
+    public boolean isSistemaBloqueado() {
+        return "S".equals(getParametros().getBloqueaSistema());
     }
 
     @Transactional(readOnly = true)
     public String obterStatus() {
-        Parametros params = repository.findById(1).orElseGet(() -> {
-            Parametros novo = new Parametros();
-            novo.setBloqueaSistema("N");
-            return repository.save(novo);
-        });
-        return params.getBloqueaSistema();
+        return getParametros().getBloqueaSistema();
     }
 
     @Auditar(tabela = "parametros", acao = "ALTERAR_BLOQUEIO", descricao = "Alterna o bloqueio do sistema", capturarEstadoAnterior = true, auditarExcecao = true, incluirRetorno = true)
@@ -45,7 +42,9 @@ public class ParametrosService {
         String novoStatus = "S".equals(params.getBloqueaSistema()) ? "N" : "S";
         params.setBloqueaSistema(novoStatus);
         repository.save(params);
-        log.info("Status do bloqueio alterado para: {}", "S".equals(novoStatus) ? "BLOQUEADO" : "LIBERADO");
-        return novoStatus == "S" ? "BLOQUEADO" : "LIBERADO";
+
+        String statusDescricao = "S".equals(novoStatus) ? "BLOQUEADO" : "LIBERADO";
+        log.info("Status do bloqueio alterado para: {}", statusDescricao);
+        return statusDescricao;
     }
 }
